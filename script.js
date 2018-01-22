d9b7d1eec7f7bdf8c0449b98a530498d5395cc64
Alex Tomlinson <alexdangersky@gmail.com>
2018-01-21 20:02:01 -0500

    erin

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 	var url = window.location.hostname;
 	var gh = "https://hootalex.github.io/chrome-extension/audio/";
 
-$("body").append("<audio autoplay src="+gh+url+".mp3></audio>");
+$("body").prepend("<audio autoplay src="+gh+url+".mp3></audio>");
 
 	});
